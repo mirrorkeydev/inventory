0ac2662305ae511075838352ffa223d2f3406ebf
mirrorkeydev <mirrorkeydev@gmail.com>
2021-10-24 16:56:11 -0700

    Add item addition and removal

--- inventory.py
+++ inventory.py
@@ -1,24 +1,28 @@
 from __future__ import annotations
 import os.path
+import googleapiclient
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from secrets import SHEET_ID, SHEET_RANGE
-from typing import Dict, List
+from typing import Dict, List, Tuple
 import argparse
 import re
 import sys
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+DELIM = '; '
 
 class Container:
-  def __init__(self, name: str, parent: Container) -> None:
+  def __init__(self, name: str, parent: Container, row_num: str) -> None:
     self.name = name
     self.parent = parent
+    self.items = []
+    self.row_num = row_num
   def __str__(self) -> str:
-    return (f"{self.parent} -> " if self.parent != None else "") + f"{self.name}"
+    return (f'{self.parent} -> ' if self.parent != None else '') + f'{self.name}'
   def __repr__(self) -> str:
     return self.__str__()
 
@@ -27,13 +31,13 @@ class Item:
     self.name = name
     self.container = container
   def __str__(self) -> str:
-    return f"'{self.name}' in {self.container}"
+    return f'\'{self.name}\' in {self.container}'
   def __repr__(self) -> str:
     return self.__str__()
 
-def fetch_data() -> List[Item] | None:
-  # So we can run this script from any directory but still reference the
-  # credentials.json file inside it, navigate to the script's location.
+def create_service():
+  # Navigate to the script's location, so we can run this script from any
+  # directory but still reference the credentials.json file inside it.
   os.chdir(os.path.dirname(os.path.realpath(sys.argv[0])))
 
   creds = None
@@ -55,8 +59,9 @@ def fetch_data() -> List[Item] | None:
       token.write(creds.to_json())
 
   service = build('sheets', 'v4', credentials=creds)
+  return service
 
-  # Call the Sheets API
+def fetch_data(service) -> Tuple[List[Item], List[Item], str] | None:
   sheet = service.spreadsheets()
   result = sheet.values().get(spreadsheetId=SHEET_ID, range=SHEET_RANGE).execute()
   values: List[List[str]] = result.get('values', [])
@@ -65,28 +70,33 @@ def fetch_data() -> List[Item] | None:
     return None
   else:
     item_list: List[Item] = []
+    uncontainered_items: str = values[0][3]
     containers: Dict[str, Container] = {}
 
     # Grab all container names and create Containers
-    for row in values:
+    for i, row in enumerate(values):
       container_name = row[0]
-      container = Container(container_name, None)
+      container = Container(container_name, None, i+2)
       containers[container.name] = container
 
     # In second run-through, set parent relationships to Containers
     for row in values:
-      container_name, parent_name, contents = row
+      container_name = row[0]
+      parent_name = row[1] if len(row) > 1 else None
+      contents = row[2] if len(row) > 2 else None
 
       container = containers[container_name]
       parent_container = containers.get(parent_name)
       if parent_container != None:
         container.parent = parent_container
 
-      items = [Item(s.strip(), container) for s in contents.split(";")]
-      for item in items:
-        item_list.append(item)
+      if contents:
+        items = [Item(s, container) for s in contents.split(DELIM)]
+        for item in items:
+          item_list.append(item)
+          container.items.append(item)
 
-  return item_list
+  return containers, item_list, uncontainered_items
 
 def search_items(search_term: str, items: List[Item]) -> List[Item]:
   results: List[Item] = []
@@ -95,37 +105,78 @@ def search_items(search_term: str, items: List[Item]) -> List[Item]:
       results.append(item)
   return results
 
-def remove_item(item: Item) -> bool:
-  pass
+def orphanize_item(service, item: Item, orphan_items: str) -> None:
+  request = service.spreadsheets().values().update(
+    spreadsheetId=SHEET_ID, range=f'D2', valueInputOption='USER_ENTERED',
+    body={
+      'range': f'D2',
+      'values': [[orphan_items + f'; {item.name}']]
+    }
+  )
+  request.execute()
 
-def main():
-  parser = argparse.ArgumentParser(prog="Inventory CLI")
-  parser.add_argument("-f", "--find", dest="f", type=str, help="find an item")
-  parser.add_argument("-r", "--remove", dest="r", type=str, help="remove an item")
+def remove_item(service, item: Item) -> None:
+  remaining_items: List[Item] = [x for x in item.container.items if x is not item]
+  request = service.spreadsheets().values().update(
+    spreadsheetId=SHEET_ID, range=f'C{item.container.row_num}',
+    valueInputOption='USER_ENTERED',
+    body={
+      'range': f'C{item.container.row_num}',
+      'values': [[DELIM.join([i.name for i in remaining_items])]]
+    }
+  )
+  request.execute()
+
+def add_item(service, item_name: str, container_name: str, containers: dict[str, Container]) -> None:
+  container = containers[container_name]
+  request = service.spreadsheets().values().update(
+    spreadsheetId=SHEET_ID, range=f'C{container.row_num}',
+    valueInputOption='USER_ENTERED',
+    body={
+      'range': f'C{container.row_num}',
+      'values': [[DELIM.join([i.name for i in container.items]) + DELIM + item_name]]
+    }
+  )
+  request.execute()
+
+def main() -> None:
+  parser = argparse.ArgumentParser(prog='Inventory CLI')
+  parser.add_argument('-f', '--find', dest='find_term', type=str, help='find an item')
+  parser.add_argument('-r', '--remove', dest='remove_term', type=str, help='remove an item')
+  parser.add_argument('-a', '--add', dest='add_term', type=str, help='add an item and specify a container.', nargs=2)
   args = parser.parse_args()
 
-  items = fetch_data()
-  if items == None:
-    print("Data source is empty!")
+  s = create_service()
+  containers, items, orphan_items = fetch_data(s)
+  if containers == None or items == None:
+    print('Data source is empty!')
     return
-  print(f"searching {len(items)} items...")
 
-  search_term = [x for x in [args.f, args.r] if x != None][0]
-  
-  results = search_items(search_term, items)
-  if not len(results):
-    print("No results found :(")
-
-  if args.f != None:
+  if args.find_term != None:
+    print(f'searching {len(containers)} containers and {len(items)} items...', end='')
+    results = search_items(args.find_term, items)
+    print(f'{len(results)} match(es)')
     for item in results:
-      print(item)
+      print(f'  - {item}')
     return
   
-  if args.r != None:
+  if args.remove_term != None:
+    print(f'searching {len(containers)} containers and {len(items)} items...', end='')
+    results = search_items(args.remove_term, items)
+    print(f'{len(results)} match(es)')
     for i, item in enumerate(results):
-      print(f"{i}: {item}")
-    item_num = input(f"Which item would you like to remove? (Enter #0-{len(results)-1}): ")
-    remove_item(results[item_num])
+      print(f'{i}: {item}')
+    item_num = input(f'Which item would you like to remove? (Enter #0-{len(results)-1}): ')
+    remove_item(s, results[int(item_num)])
+    orphanize_item(s, item, orphan_items)
+    print(f'Item \'{args.remove_term}\' removed!')
+    return
+
+  if args.add_term != None:
+    item_name, container_name = args.add_term
+    add_item(s, item_name, container_name, containers)
+    print(f'Item \'{item_name}\' added to container {container_name}!')
+    return
 
 if __name__ == '__main__':
   main()
